6c6488cd87e592f0834afa0b9c73fd75cd61aa01
BG <gbatkhuyag@gmail.com>
2024-12-08 11:01:32 +0800

    Create cloud.php

---
+++ lib/cloud.php
@@ -0,0 +1,10 @@
+<?php
+// languages/english.php
+
+$_LANG['letsEncryptSSL'] = "Let's Encrypt SSL Certificate";
+$_LANG['domain'] = "Domain";
+$_LANG['email'] = "Email";
+$_LANG['issueSSL'] = "Issue SSL Certificate";
+$_LANG['txtRecordFailure'] = "Failed to add DNS TXT record for validation.";
+$_LANG['sslIssued'] = "SSL Certificate successfully issued!";
+$_LANG['apiKeyError'] = "API keys are not available for Hetzner Cloud or DNS.";
